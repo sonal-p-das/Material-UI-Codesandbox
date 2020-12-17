03bb404f7707f88c683269d9efc6b6e2388be6a9
Sonal Das <sonald@alumni.cmu.edu>
2020-12-16 23:35:03 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React from "react";
+import ToolbarComponent from "./components/Toolbar/Toolbar";
+import DrawerComponent from "./components/Drawer/Drawer";
+
+class App extends React.Component {
+  state = {
+    left: false
+  };
+
+  toggleDrawer = () => {
+    // if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+    //   return;
+    // }
+
+    this.setState({ left: false });
+  };
+
+  openDrawer = () => {
+    this.setState({
+      left: true
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <ToolbarComponent openDrawerHandler={this.openDrawer} />
+        <DrawerComponent
+          left={this.state.left}
+          toggleDrawerHandler={this.toggleDrawer}
+        />
+      </div>
+    );
+  }
+}
+export default App;
